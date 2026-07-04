ae6f5c6c6166f0cb93e457c6dc2a4f50d7d68559
aletre <aletre34@gmail.com>
2026-07-04 07:42:58 -0500

    feat: record study day on init; gamification wiring (service worker v6)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service worker: precache del núcleo para funcionamiento offline.
-var CACHE = "english-kids-v5";
+var CACHE = "english-kids-v6";
 var CORE = [
   ".",
   "index.html",
@@ -13,6 +13,7 @@ var CORE = [
   "js/study.js",
   "js/quiz.js",
   "js/memory.js",
+  "js/gamification.js",
   "js/ui.js",
   "js/app.js",
   "manifest.webmanifest",
